2f9067426b68bf25a6dd6bb02270f08bf6939a32
unknown <chughsiddharth25@gmail.com>
2020-09-02 19:59:02 +0530

    Updates

---
+++ precache-manifest.d565cc7dd73b26a500f01aaa8ceaaa7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f91d999c3eb07b5a641",
+    "url": "/catclickreact/static/css/main.f7fba191.chunk.css"
+  },
+  {
+    "revision": "3f91d999c3eb07b5a641",
+    "url": "/catclickreact/static/js/main.3f91d999.chunk.js"
+  },
+  {
+    "revision": "6a48360ba1437a7443c2",
+    "url": "/catclickreact/static/js/1.6a48360b.chunk.js"
+  },
+  {
+    "revision": "81f5374721bde72d9d10",
+    "url": "/catclickreact/static/js/runtime~main.81f53747.js"
+  },
+  {
+    "revision": "9e50cc8dd25f357b13825c5a44363f0b",
+    "url": "/catclickreact/index.html"
+  }
+];
